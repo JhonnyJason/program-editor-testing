887f0f3b620246a8fbd23cffae454f86afb7381a
Lenard Frommelt <lenny09918050@googlemail.com>
2020-02-11 15:45:56 +0100

    safe sync

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # remove stuff
-systemctl stop program-manager-testing.socket
-systemctl stop program-manager-testing.service
-rm /run/program-manager-testing.sk
+systemctl stop program-editor-testing.socket
+systemctl stop program-editor-testing.service
+rm /run/program-editor-testing.sk
 # cop stuff
-cp program-manager-testing.service /etc/systemd/system/
-cp program-manager-testing.socket /etc/systemd/system/
-cp nginx-config /etc/nginx/servers/program-manager-testing
+cp program-editor-testing.service /etc/systemd/system/
+cp program-editor-testing.socket /etc/systemd/system/
+cp nginx-config /etc/nginx/servers/program-editor-testing
 # reload restard daemons and socket
 systemctl daemon-reload
-systemctl start program-manager-testing.socket
+systemctl start program-editor-testing.socket
 nginx -s reload
